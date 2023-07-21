1ebd644e54b818a7dfbd6078101b6843e5ae3aea
wujian <jianwu6@iflytek.com>
2023-07-21 13:41:11 +0800

    [redis] 自定义 redisTemplate

--- spring-boot-data/src/main/java/com/demozi/spring/boot/data/redis/UserRedisTemplate.java
+++ spring-boot-data/src/main/java/com/demozi/spring/boot/data/redis/UserRedisTemplate.java
@@ -1,51 +1,27 @@
 package com.demozi.spring.boot.data.redis;
 
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import com.demozi.spring.boot.data.redis.entity.User;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 /**
- * @author wujian  2023/7/13 20:35
+ * @author wujian  2023/7/21 10:21
  */
 @Component
 public class UserRedisTemplate {
 
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    private final StringRedisTemplate redisTemplate;
-
-    public UserRedisTemplate(StringRedisTemplate redisTemplate) {
+    public UserRedisTemplate(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
-    public void save(String key, String value) {
-        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
-        ops.set(key, value);
+    public void saveUser(String key, User user) {
+        this.redisTemplate.opsForValue().set(key, user);
     }
 
-    public void saveWithExpire(String key, String value, long expireMs) {
-        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
-        ops.set(key, value, expireMs, TimeUnit.MILLISECONDS);
-    }
-
-
-    public String get(String key) {
-        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
-        return ops.get(key);
-    }
-
-    public void delete(String key) {
-        this.redisTemplate.delete(key);
-    }
-
-    public void saveHashMap(String key, Map<String, ?> value) {
-        this.redisTemplate.opsForHash().putAll(key, value);
-    }
-
-    public Map<Object, Object> getHashMap(String key) {
-        return this.redisTemplate.opsForHash().entries(key);
+    public User getUser(String key) {
+        return (User) this.redisTemplate.opsForValue().get(key);
     }
 
 
--- spring-boot-data/src/test/java/com/demozi/spring/boot/data/redis/UserRedisTemplateTest.java
+++ spring-boot-data/src/test/java/com/demozi/spring/boot/data/redis/UserRedisTemplateTest.java
@@ -1,64 +1,36 @@
 package com.demozi.spring.boot.data.redis;
 
+import com.demozi.spring.boot.data.redis.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
- *
- * @author wujian  2023/7/13 20:54
+ * @author wujian  2023/7/21 10:32
  */
 @SpringBootTest
-class UserRedisTemplateTest {
+public class UserRedisTemplateTest {
 
     @Autowired
     private UserRedisTemplate redisTemplate;
 
     @Test
-    public void test() {
-        String key = "name";
-        String value = "zhangsan";
-        redisTemplate.save(key, value);
+    public void save() {
+        User user = new User();
+        user.setAge(20);
+        user.setName("zhangsan");
+        Map<String, Object> props = new HashMap<>();
+        props.put("int", 11);
+        props.put("string", "txt");
+        user.setProps(props);
+        user.setVersion(Arrays.asList(1, 9));
+        redisTemplate.saveUser("zs", user);
 
-        assertEquals(value, redisTemplate.get(key));
-
-        System.out.println(redisTemplate.get(key));
-
-        redisTemplate.delete(key);
-        assertNull(redisTemplate.get(key));
-    }
-
-    @Test
-    public void testHashMap() {
-        String key = "map";
-        Map<String, String> value = new HashMap<>();
-        value.put("age", "20");
-        value.put("name", "zhangsan");
-        redisTemplate.saveHashMap(key, value);
-
-        Map<Object, Object> hashMap = redisTemplate.getHashMap(key);
-        hashMap.forEach((k,v) -> {
-            System.out.printf("key: " + k + "----value: " + v);
-        });
-    }
-
-    @Test
-    public void testSaveWithExpire() throws InterruptedException {
-        String key = "expire";
-        String value = "33";
-        redisTemplate.saveWithExpire(key, value, 3000);
-        System.out.println(redisTemplate.get(key));
-
-        TimeUnit.SECONDS.sleep(4);
-        System.out.println(redisTemplate.get(key));
-        assertNull(redisTemplate.get(key));
+        System.out.println(redisTemplate.getUser("zs"));
     }
 
 }
