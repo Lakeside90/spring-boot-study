71f50075a2f835e55f3973f0d7388732f3edc085
wujian <jianwu6@iflytek.com>
2023-08-11 17:35:32 +0800

    [mysql]  mybatis

---
+++ spring-boot-data/src/main/java/com/demozi/spring/boot/data/mysql/IUserDao2.java
@@ -0,0 +1,9 @@
+package com.demozi.spring.boot.data.mysql;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author wujian  2023/8/10 10:34
+ */
+public interface IUserDao2 extends BaseMapper<User> {
+}
